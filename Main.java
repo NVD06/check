dfa80d4bab0ece67f5bafdd6bca8676ea1055f6d
nvd <concoko35@gmail.com>
2024-04-19 14:18:32 +0700

    main

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 5;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " chan");
+            } else {
+                System.out.println(i + " le");
+            }
+        }
+    }
+}
